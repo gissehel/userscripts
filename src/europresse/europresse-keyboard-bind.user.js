aaf29f026d96f8f3bbf56e7871e8a0652abe7800
gissehel <public-dev-git-eudix@gissehel.org>
2025-11-20 14:36:27 +0100

    europresse : first draft for key bindings

---
+++ src/europresse/europresse-keyboard-bind.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @version      1.0.0
+// @description  europresse-keyboard-bind
+// ==/UserScript==
+
+// @import{registerDomNodeMutatedUnique}
+// @import{registerEventListerner}
+// @import{getElements}
+
+registerDomNodeMutatedUnique(() => getElements('#currentDoc.panel'), (close_button) => {
+    const next_button = getElements('#nextPdf')[0]
+    const prev_button = getElements('#prevPdf')[0]
+    const zoom_in_button = getElements('#zoomin')[0]
+    const zoom_out_button = getElements('#zoomout')[0]
+
+    registerEventListerner(document.body, 'keydown', (event) => {
+        if (event.key === 'ArrowRight') {
+            next_button.click()
+            return true
+        }
+        if (event.key === 'ArrowLeft') {
+            prev_button.click()
+            return true
+        }
+        if (event.key === '+') {
+            zoom_in_button.click()
+            return true
+        }
+        if (event.key === '-') {
+            zoom_out_button.click()
+            return true
+        }
+        return false
+    })
+    
+
+})
