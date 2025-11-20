376a937cc1e19b3cc3c458eb86ed92cbbf251906
gissehel <public-dev-git-eudix@gissehel.org>
2025-11-20 15:01:25 +0100

    More keys

--- src/europresse/europresse-keyboard-bind.user.js
+++ src/europresse/europresse-keyboard-bind.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version      1.0.0
+// @version      1.0.1
 // @description  europresse-keyboard-bind
 // ==/UserScript==
 
@@ -12,22 +12,31 @@ registerDomNodeMutatedUnique(() => getElements('#currentDoc.panel'), (close_butt
     const prev_button = getElements('#prevPdf')[0]
     const zoom_in_button = getElements('#zoomin')[0]
     const zoom_out_button = getElements('#zoomout')[0]
-
+    const pdf_pages_panel_btn = getElements('span.pdf-pages-panel-btn')[0]
+    const pdf_pages_panel_btn_close = getElements('i.pdf-pages-panel-btn')[0]
     registerEventListener(document.body, 'keydown', (event) => {
-        if (event.key === 'ArrowRight') {
+        if (event.key === 'ArrowRight' || event.code === 'KeyL' || event.code === 'Space') {
             next_button.click()
             return true
         }
-        if (event.key === 'ArrowLeft') {
+        if (event.key === 'ArrowLeft' || event.code === 'KeyH') {
             prev_button.click()
             return true
         }
-        if (event.key === '+') {
+        if (event.key === '+' || event.code === 'KeyJ') {
             zoom_in_button.click()
             return true
         }
-        if (event.key === '-') {
+        if (event.key === '-' || event.code === 'KeyK') {
             zoom_out_button.click()
+            return true
+        }
+        if (event.code === 'KeyP') {
+            pdf_pages_panel_btn.click()
+            return true
+        }
+        if (event.code === 'KeyX') {
+            pdf_pages_panel_btn_close.click()
             return true
         }
         return false
