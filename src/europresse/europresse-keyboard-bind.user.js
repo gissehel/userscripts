3444f1b6a7a4911ca067ffc4d3dd5ae89d47fbc6
gissehel <public-dev-git-capella@gissehel.org>
2025-12-03 15:06:58 +0100

    Use fine grained zoom

--- src/europresse/europresse-keyboard-bind.user.js
+++ src/europresse/europresse-keyboard-bind.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version      1.0.13
+// @version      1.0.14
 // @description  europresse-keyboard-bind
 // ==/UserScript==
 
@@ -13,33 +13,41 @@ const getKeyKey = ({ code, key, ctrlKey, shiftKey, altKey, metaKey }) => {
 }
 
 const removePx = (x) => {
-  if (x.substr(-2) == "px") { return x.substr(0, x.length-2) } else { return x }
+    if (x.substr(-2) == "px") { return x.substr(0, x.length - 2) } else { return x }
 }
 
 registerDomNodeMutatedUnique(() => getElements('#currentDoc.panel'), (close_button) => {
     const next_button = getElements('#nextPdf')[0]
     const prev_button = getElements('#prevPdf')[0]
-    const zoom_in_button = getElements('#zoomin')[0]
-    const zoom_out_button = getElements('#zoomout')[0]
+    // const zoom_in_button = getElements('#zoomin')[0]
+    // const zoom_out_button = getElements('#zoomout')[0]
     const reset_zoom_button = getElements('#reset')[0]
     const pdf_pages_panel_btn = getElements('span.pdf-pages-panel-btn')[0]
     const downloadImage = () => downloadDataUrl(getElements('.imagePdf')[0].src, `europresse-${_docNameList[_docIndex]}`)
     const moveDirection = (attrName, delta) => {
         const viewer = getElements('img.viewer-move')[0];
+        window.viewer = viewer;
         if (attrName === 'marginLeft') {
-            _pdfViewer.moveTo(viewer.offsetLeft + delta,viewer.offsetTop)
+            _pdfViewer.moveTo(viewer.offsetLeft + delta, viewer.offsetTop)
         } else if (attrName === 'marginTop') {
-            _pdfViewer.moveTo(viewer.offsetLeft,viewer.offsetTop + delta)
+            _pdfViewer.moveTo(viewer.offsetLeft, viewer.offsetTop + delta)
         }
         // if (viewer) {
         //     viewer.style[attrName] = `${Number(removePx(viewer.style[attrName]))+delta}px` 
         // }
 
     };
+    const zoomGoldenRatio = 1.6180339887;
+    const zoom_in_action = () => {
+        _pdfViewer.zoomTo(_ratio * zoomGoldenRatio);
+    }
+    const zoom_out_action = () => {
+        _pdfViewer.zoomTo(_ratio / zoomGoldenRatio);
+    }
     const next_action = () => next_button.click();
     const prev_action = () => prev_button.click();
-    const zoom_in_action = () => zoom_in_button.click();
-    const zoom_out_action = () => zoom_out_button.click();
+    // const zoom_in_action = () => zoom_in_button.click();
+    // const zoom_out_action = () => zoom_out_button.click();
     const reset_zoom_action = () => reset_zoom_button.click();
     const togglePdfPagesPanel = () => pdf_pages_panel_btn.click();
     const moveLeft = () => moveDirection('marginLeft', -10);
@@ -48,8 +56,8 @@ registerDomNodeMutatedUnique(() => getElements('#currentDoc.panel'), (close_butt
     const moveDown = () => moveDirection('marginTop', 10);
     const actions = {
     }
-    const addAction = (action, ...keyStructs ) => {
-        keyStructs.forEach( (keyStruct) => {
+    const addAction = (action, ...keyStructs) => {
+        keyStructs.forEach((keyStruct) => {
             actions[getKeyKey(keyStruct)] = action
         })
     }
@@ -107,9 +115,9 @@ registerDomNodeMutatedUnique(() => getElements('#currentDoc.panel'), (close_butt
 
     registerEventListener(document.body, 'keydown', (event) => {
         let result = false;
-        ['code','key'].forEach((prop) => {
-            
-            const {code, key, ctrlKey, shiftKey, altKey, metaKey} = event
+        ['code', 'key'].forEach((prop) => {
+
+            const { code, key, ctrlKey, shiftKey, altKey, metaKey } = event
             const keyKey = getKeyKey({ code, key, ctrlKey, shiftKey, altKey, metaKey, [prop]: undefined })
             console.log('keyKey', keyKey)
             if (actions[keyKey]) {
