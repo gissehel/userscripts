34480a03bff8007492ab7fe091db97e28752ad4e
gissehel <public-dev-git-capella@gissehel.org>
2025-12-03 11:54:02 +0100

    europresse-allow-big-vertical-screen

---
+++ src/europresse/europresse-allow-big-vertical-screen.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @version      1.0.0
+// @description  europresse-allow-big-vertical-screen
+// ==/UserScript==
+
+// @import{addStyle}
+
+addStyle('#pdf, #pdfDocument, .docContainer, .panel { height: 100%; }')
