b4f4461981376d83cb2c6ba05dea1fa1bdec9922
gissehel <public-dev-git-capella@gissehel.org>
2025-12-05 15:35:55 +0100

    Fix async bug

--- src/europresse/europresse-ensure-cache-for-already-downloaded-pages.user.js
+++ src/europresse/europresse-ensure-cache-for-already-downloaded-pages.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version      1.0.35
+// @version      1.0.36
 // @description  europresse-ensure-cache-for-already-downloaded-pages
 // ==/UserScript==
 
@@ -180,37 +180,36 @@ exportOnWindow({ ensureCurrentPageCached });
 // #endregion
 
 // #region original functions overrides
-renderPdf = async (n, t) => {
+const renderPdf = async (n, t) => {
     await showWaitingScreen();
     for (var u = "", r = $(".viewer-move").length !== 0 ? $(".viewer-move").offset() : null, i = 0; i < n; i++) {
-        ensureImageCached(i, t, n).then((n) => {
-            u += "<div id='rawimagewrapper'><img id='imagePdf" + i + "' class='imagePdf' src='data:image/png;base64," + n + "' /><\/div>";
-            $("#pdfDocument").html(u);
-            _pdfViewer = new Viewer(document.getElementById("rawimagewrapper"), {
-                inline: !0,
-                button: !1,
-                title: !1,
-                toolbar: !1,
-                transition: !1,
-                navbar: !1,
-                zoomRatio: .7,
-                minZoomRatio: .25,
-                maxZoomRatio: 10,
-                viewed: function () {
-                    _ratio !== null && _pdfViewer.zoomTo(_ratio);
-                    r != null && _pdfViewer.moveTo(r.left, r.top)
-                },
-                zoomed: function (n) {
-                    _ratio = n.detail.ratio
-                }
-            })
+        const data = await ensureImageCached(i, t, n)
+        u += "<div id='rawimagewrapper'><img id='imagePdf" + i + "' class='imagePdf' src='data:image/png;base64," + data + "' /><\/div>";
+        $("#pdfDocument").html(u);
+        _pdfViewer = new Viewer(document.getElementById("rawimagewrapper"), {
+            inline: !0,
+            button: !1,
+            title: !1,
+            toolbar: !1,
+            transition: !1,
+            navbar: !1,
+            zoomRatio: .7,
+            minZoomRatio: .25,
+            maxZoomRatio: 10,
+            viewed: function () {
+                _ratio !== null && _pdfViewer.zoomTo(_ratio);
+                r != null && _pdfViewer.moveTo(r.left, r.top)
+            },
+            zoomed: function (n) {
+                _ratio = n.detail.ratio
+            }
         })
     }
     await hideWaitingScreen();
 }
 exportOnWindow({ renderPdf });
 
-async function openPdf(n) {
+const openPdf = async (n) => {
     _animTimer = setTimeout(function () {
         showWaitingAnim()
     }, _animSpeed);
