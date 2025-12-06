ac5757902476f8ed6ad4a94d73da98c549ca7b61
gissehel <public-dev-git-capella@gissehel.org>
2025-12-06 17:25:50 +0100

    Add getUrl{Hash/Search}Params snippets

---
+++ snippet/getUrlSearchParams.js
@@ -0,0 +1,7 @@
+/**
+ * Return an object representing the URL search parameters (ex: ?key1=value1&key2=value2 in the URL would return { key1: 'value1', key2: 'value2' })
+ * @returns {Object} The parameters as key/value pairs
+ */
+const getUrlSearchParams = () => {
+    return Object.fromEntries(document.location.search.slice(1).split('&').map(item=>[item.slice(0,item.indexOf('=')), item.slice(item.indexOf('=')+1)]).filter(entry=>entry[0].length));
+}
