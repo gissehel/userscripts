99352d37d9080d7a4d12bb1c3c5d80c7a273a43b
gissehel <public-dev-git-capella@gissehel.org>
2026-01-21 23:18:12 +0100

    Fix problems with JSZip

--- snippet/downloadZip.js
+++ snippet/downloadZip.js
@@ -1,27 +1,4 @@
-// @grant{unsafeWindow}
-const loadInUnsafeWindow = (url) => {
-    return new Promise((resolve, reject) => {
-        if (unsafeWindow.JSZipPromise) {
-            resolve(unsafeWindow.JSZip);
-            return;
-        }
-        const script = document.createElement('script');
-        script.src = url;
-        script.onload = () => {
-            resolve(unsafeWindow.JSZip);
-        };
-        script.onerror = () => {
-            reject(new Error(`Failed to load script: ${url}`));
-        }
-        document.head.appendChild(script);
-    });
-};
-
-const JSZipPromise = loadInUnsafeWindow('https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js')
-if (!unsafeWindow.JSZipPromise) {
-    unsafeWindow.JSZipPromise = JSZipPromise;
-}
-
+// @require{https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js}
 /**
  * Download a zip file with the given filename and content provided by the async generator
  * 
@@ -29,10 +6,15 @@ if (!unsafeWindow.JSZipPromise) {
  * @param {() => AsyncGenerator<{path: string, data: Uint8Array<ArrayBuffer>}>} contentProvider 
  */
 const downloadZip = async (fileName, contentProvider) => {
-    const JSZip = await JSZipPromise
     const zip = new JSZip();
-    for await (const { path, data } of contentProvider()) {
-        zip.file(path, data);
+    for await (const { path, data, date } of contentProvider()) {
+        const options = {};
+
+        if (date) {
+            options.date = date;
+        }
+        
+        zip.file(path, data, options);
     }
     const content = await zip.generateAsync({ type: "blob" });
     const link = document.createElement('a');
