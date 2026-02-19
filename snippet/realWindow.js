8f7d31d3e64bc1af93b60fc46ddf0c491f0b3dd7
gissehel <public-dev-git-capella@gissehel.org>
2026-02-19 12:16:15 +0100

    Better implementation of realWindow

--- snippet/realWindow.js
+++ snippet/realWindow.js
@@ -1,8 +1,1 @@
-let realWindow = null;
-try {
-    realWindow = unsafeWindow;
-    console.log('Using unsafeWindow as realWindow');
-} catch (e) {
-    realWindow = window;
-    console.log('Using window as realWindow');
-}
+const realWindow = typeof window.unsafeWindow === 'undefined' ? window : window.unsafeWindow
