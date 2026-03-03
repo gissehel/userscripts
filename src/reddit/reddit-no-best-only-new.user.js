655d18996ecb07d25de41aefa3b708fa96c83cec
gissehel <public-dev-git-eudix@gissehel.org>
2026-03-03 15:21:37 +0100

    FIX : Fix loading problem when reddit url change too fast

--- src/reddit/reddit-no-best-only-new.user.js
+++ src/reddit/reddit-no-best-only-new.user.js
@@ -1,9 +1,11 @@
+// @import{delay}
 // @import{registerLocationChange}
 
 const main = async () => {
-    registerLocationChange((/**@type{Location}*/ location) => {
+    registerLocationChange(async (/**@type{Location}*/ location) => {
         const locationParts = location.pathname.split('/')
         if (locationParts.length === 4 && locationParts[1] === 'r' && locationParts[0] === '' && locationParts[3] === '') {
+            await delay(2000)
             window.location.pathname = `/r/${locationParts[2]}/new/`;
         }
     })
