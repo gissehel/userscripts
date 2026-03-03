fc4d3753bc3babb7a118011fe5e6d5e588044ccd
gissehel <public-dev-git-capella@gissehel.org>
2026-03-03 19:30:19 +0100

    Allow to show/hide generic panel from the menu

--- snippet/createSsmGenericPanel.js
+++ snippet/createSsmGenericPanel.js
@@ -4,17 +4,25 @@
 // @import{createElementExtended}
 // @import{createIconLinkAction}
 // @import{ICONS}
+// @import{registerMenuCommand}
 
 /**
  * Creates a generic panel
  * @param {string} localName The local storage name
  * @param {()=>Promise<HTMLElement[]>} getPanelContent The function to get the panel content
+ * @param {Object} [options] Additional options
+ * @param {(newPosition: string) => void} [options.onPanelPositionChanged] A callback called when the panel position changes
+ * @param {(swapGenericPanel: () => void) => void} [options.setSwapGenericPanelFunction] A function to get the swap generic panel function, which can be used to toggle the panel visibility
  */
-const createSsmGenericPanel = async (localName, getPanelContent) => {
+const createSsmGenericPanel = async (localName, getPanelContent, options) => {
+    if (!options) {
+        options = {};
+    }
     const PANEL_POSITION = {
         RIGHT: 'right',
         LEFT: 'left',
         MINI: 'mini',
+        NONE: 'none',
     }
 
     const getNextPanelPosition = (position) => {
@@ -22,6 +30,7 @@ const createSsmGenericPanel = async (localName, getPanelContent) => {
             case PANEL_POSITION.RIGHT: return PANEL_POSITION.LEFT;
             case PANEL_POSITION.LEFT: return PANEL_POSITION.MINI;
             case PANEL_POSITION.MINI: return PANEL_POSITION.RIGHT;
+            case PANEL_POSITION.NONE: return PANEL_POSITION.RIGHT;
         }
         return PANEL_POSITION.RIGHT;
     }
@@ -64,6 +73,11 @@ const createSsmGenericPanel = async (localName, getPanelContent) => {
                     panel.style.right = '10px';
                     panel.style.left = 'unset';
                 }
+                if (newValue === PANEL_POSITION.NONE) {
+                    panel.style.display = 'none';
+                } else {
+                    panel.style.display = 'block';
+                }
             });
         },
     }));
@@ -85,4 +99,25 @@ const createSsmGenericPanel = async (localName, getPanelContent) => {
             },
         }),
     )
+
+    const swapGenericPanel = async () => {
+        if (panelPosition.value === PANEL_POSITION.NONE) {
+            panelPosition.value = PANEL_POSITION.RIGHT;
+        } else {
+            panelPosition.value = PANEL_POSITION.NONE;
+        }
+    }
+
+    registerMenuCommand('Show/Hide generic panel', async () => {
+        if (swapGenericPanel) {
+            await swapGenericPanel();
+        }
+    })
+
+    if (options.onPanelPositionChanged) {
+        panelPosition.register(options.onPanelPositionChanged);
+    }
+    if (options.setSwapGenericPanelFunction) {
+        options.setSwapGenericPanelFunction(swapGenericPanel);
+    }
 }
