9301e8f623c8671a303fa612971a9e695454bfc5
gissehel <public-dev-git-capella@gissehel.org>
2026-08-26 10:52:13 +0200

    Add icon + change parameter name

--- src/generic/disable-window-open.user.js
+++ src/generic/disable-window-open.user.js
@@ -1,3 +1,8 @@
+// ==UserScript==
+// @description  Disable window.open calls by hosts
+// @icon         data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgZmlsbD0iIzAwMDAwMCIgdmlld0JveD0iMCAwIDI1NiAyNTYiPjxwYXRoIGQ9Ik0xODQsNzJINDBBMTYsMTYsMCwwLDAsMjQsODhWMjAwYTE2LDE2LDAsMCwwLDE2LDE2SDE4NGExNiwxNiwwLDAsMCwxNi0xNlY4OEExNiwxNiwwLDAsMCwxODQsNzJabTAsMTI4SDQwVjg4SDE4NFYyMDBaTTIzMiw1NlYxNzZhOCw4LDAsMCwxLTE2LDBWNTZINjRhOCw4LDAsMCwxLDAtMTZIMjE2QTE2LDE2LDAsMCwxLDIzMiw1NloiPjwvcGF0aD48L3N2Zz4=
+// ==/UserScript==
+
 // @import{getPersistentParameterValueBoolean}
 // @import{PERSISTENT_PARAMETER_SCOPE}
 // @import{realWindow}
@@ -9,9 +14,9 @@ const main = async () => {
         console.log('   Arguments tried:', args);
         return null;
     }
-    const enableWindowOpen = await getPersistentParameterValueBoolean('allow-window-open', true, {
+    const enableWindowOpen = await getPersistentParameterValueBoolean('opening-popups', true, {
         scope: PERSISTENT_PARAMETER_SCOPE.BY_HOST,
-        dontStoreDefault: true,      
+        dontStoreDefault: true,
     });
     enableWindowOpen.registerAndCall(async (newValue) => {
         if (newValue) {
@@ -20,9 +25,9 @@ const main = async () => {
             }
         } else {
             if (realWindow.open !== windowOpenMock) {
-                realWindow.open = windowOpenMock; 
+                realWindow.open = windowOpenMock;
             }
-        }   
+        }
     })
 }
 
